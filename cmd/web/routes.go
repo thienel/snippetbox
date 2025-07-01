9063caa2c5d9e60f447543e83e16e38224d7d54d
thienel <lethien.dev@gmail.com>
2025-07-02 00:00:27 +0700

    Complete to chapter 11.2

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,11 +21,27 @@ func (app *application) routes() http.Handler {
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
-	router.Handler(http.MethodGet, "/home", dynamic.ThenFunc(app.home))
-	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
-	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+	router.Handler(http.MethodGet, "/",
+		dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/home",
+		dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/snippet/view/:id",
+		dynamic.ThenFunc(app.snippetView))
+	router.Handler(http.MethodGet, "/snippet/create",
+		dynamic.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create",
+		dynamic.ThenFunc(app.snippetCreatePost))
+
+	router.Handler(http.MethodGet, "/user/signup",
+		dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup",
+		dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login",
+		dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login",
+		dynamic.ThenFunc(app.userLogoutPost))
+	router.Handler(http.MethodPost, "/user/logout",
+		dynamic.ThenFunc(app.userLogoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
