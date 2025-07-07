7ce07edfdfe61d32a70ad21e1949cb0b33a0e551
thienel <lethien.dev@gmail.com>
2025-07-07 21:00:47 +0700

    Complete to chapter 14.3

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+	"thienel/lets-go/internal/assert"
+)
+
+func TestPing(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.get(t, "/ping")
+
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
+}
